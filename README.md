33b3f7e688553d3dddd28de73653f0d0e5efc482
Fernando Gonzalez <fgonz009@ucr.edu>
2017-10-31 15:05:28 -0700

    An updated list of movies.

--- README.md
+++ README.md
@@ -15,14 +15,16 @@ Table of Contents
 
 1. File Description
 	1. media.py - File containing the Movie class. This class stores movie title, story line, poster image, and movie trailer URL.
-	2. fresh_tomatoes.html - File containing
-	3. entertainment_center.py - 
+	2. fresh_tomatoes.html - File containing a Python Module which takes a list of movie instances and displays them in a HTML file. 
+	3. entertainment_center.py - File containing six movie instances which will be displayed in the movie website.
 
 2. Python Environment
 	1. Python 2.7.14
 	
 3. Execution Steps
-	1. Fork Repository
-	2. From terminal run the following command when if you are currently in the directory with all the files $ python media.py
+	1. Fork Repository:
+		1.[Github Repository](https://github.com/Fernie-Hacks/Movie-Trailer)
+	2. From terminal run the following command (Assuming working directory contains all the project files that were forked in previous step). 
+		1. $ python media.py
 	3. Alternatively start Python IDLE environment
-	4. Open media.py and Run Module under the ‘Run’ Tab option.
+	4. Open media.py and select ‘Run Module’ which is under the ‘Run’ tab option.
--- entertainment_center.py
+++ entertainment_center.py
@@ -6,7 +6,7 @@ import fresh_tomatoes
 toy_story = media.Movie("Toy Story",
                         "A story of a boy and his toys that come to life",
                         "https://upload.wikimedia.org/wikipedia/en/1/13/Toy_Story.jpg",
-                        "https://www.youtube.com/watch?v=KYz2wyBy3kc",)
+                        "https://www.youtube.com/watch?v=KYz2wyBy3kc")
 avatar = media.Movie("Avatar",
                      "A marine on an alien planet",
                      "https://upload.wikimedia.org/wikipedia/en/b/b0/Avatar-Teaser-Poster.jpg",
@@ -14,9 +14,21 @@ avatar = media.Movie("Avatar",
 donnie_darko = media.Movie("Donnie Darko",
                            "Time Traveling to save the world",
                            "https://upload.wikimedia.org/wikipedia/en/d/db/Donnie_Darko_poster.jpg",
-                           "https://www.youtube.com/watch?v=ZZyBaFYFySk",)
+                           "https://www.youtube.com/watch?v=ZZyBaFYFySk")
+pulp_fiction = media.Movie("Pulp Fiction",
+                           "American black comedy neo-noir crime film",
+                           "https://upload.wikimedia.org/wikipedia/en/3/3b/Pulp_Fiction_%281994%29_poster.jpg",
+                           "https://www.youtube.com/watch?v=s7EdQ4FqbhY")
+inglourious_basterds = media.Movie("Inglourious Basterds",
+                                   "A fictional alternate history story of two plots to assassinate Nazi Germany's political leadership.",
+                                   "https://upload.wikimedia.org/wikipedia/en/c/c3/Inglourious_Basterds_poster.jpg",
+                                   "https://www.youtube.com/watch?v=6AtLlVNsuAc")
+source_code = media.Movie("Source Code",
+                          "A captain who is sent into a computed reality to find a bomber.",
+                          "https://upload.wikimedia.org/wikipedia/en/e/e5/Source_Code_Poster.jpg",
+                          "https://www.youtube.com/watch?v=NkTrG-gpIzE")
 
 #Creates an array of Movie instances
-movies = [toy_story, avatar, donnie_darko]
+movies = [toy_story, avatar, donnie_darko, pulp_fiction, inglourious_basterds, source_code]
 #Call the fresh_tomatoes open_movie_page() function which takes the list
 fresh_tomatoes.open_movies_page(movies)
